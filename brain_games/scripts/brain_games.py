f5a32dd282d0c1621b9e4a5f3ba3fa095c004846
Fedor Pereverzev <maphtown@gmail.com>
2022-03-19 04:03:24 +0400

    работает только если вызывать скрипт напрямую, в системе brain-games не обновляется

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
 
+from BRAIN_GAMES.cli import welcome_user
+
 def main():
     print('Welcome to the Brain Games!')
 
 if __name__ == '__main__':
     main()
+    welcome_user()
